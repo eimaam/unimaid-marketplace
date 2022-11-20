7da75f37403f72279eec56e8183d964c65ab7efc
eimaam <imamddahir@gmail.com>
2022-11-20 04:19:59 +0100

    bettered input type button display on mobile

--- src/Routes/RoutesWithNavBar.js
+++ src/Routes/RoutesWithNavBar.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 export const RoutesWithNavBar = () => {
   return (
     <>
         <Nav />
-        
-
+        <Outlet />
     </>
   )
 }
