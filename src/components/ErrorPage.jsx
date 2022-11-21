fd52a62d132de0eed1cbf7b3bac88aff96ea680a
eimaam <imamddahir@gmail.com>
2022-11-21 19:39:48 +0100

    created ErrorPage

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const ErrorPage = () => {
+  return (
+    <div className='container'>
+        <div className='container--item'>
+            <h1>Oooops! 404 </h1>
+            <h2>The page you're looking for is either broken or does not exist! 🤮</h2>
+
+        </div>
+    </div>
+  )
+}
