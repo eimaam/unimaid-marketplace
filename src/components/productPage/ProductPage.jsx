3a369e2e46c83d59a464efdf3575618c2728d5f0
eimaam <imamddahir@gmail.com>
2022-12-03 13:01:35 +0100

    edited seller card display on mobile and resized item image to size

--- src/components/productPage/ProductPage.jsx
+++ src/components/productPage/ProductPage.jsx
@@ -15,6 +15,7 @@ import { database } from '../../firebaseConfig'
 import { ItemSpec } from './ItemSpec'
 import { MoreCategoryItems } from './MoreCategoryItems'
 import { ItemStats } from './ItemStats'
+import { SafetyMeasures } from './SafetyMeasures'
 
 export const ProductPage = () => {
   const { userRef, adsRef, loading, setLoading } = useAuth()
@@ -136,17 +137,7 @@ export const ProductPage = () => {
         />
         
         {/* ---------safety measures--------- */}
-        <div className='safety--measures'>
-          <h3>Safety Measures</h3>
-          <p>Few safety tips for a favourable Trade</p>
-          <ul>
-            <li>Make sure to meet Seller in a public place, if seller stays Off campus, request he comes to it if possible. </li>
-            <li>Make sure to check the item thoroughly before purchase</li>
-            <li>If payment is via Mobile transfer, Seller should confirm receipt of payment to his account via his/her Bank's Mobile App or USSD before releasing item[0]. </li>
-            <li>If item is a Smartphone, Laptop, Gadget or Electronics, Buyer should test charging port, camera (if item is a mobile device), before purchase. </li>
-            
-          </ul>
-        </div>
+        <SafetyMeasures />
 
         {/* ---------seller profile details--------- */}
         <SellerCard 
