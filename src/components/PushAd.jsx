a89389d7c3ddbb6bf07af06ff5c7280d52979891
eimaam <imamddahir@gmail.com>
2022-12-06 22:29:59 +0100

    add: created component for pushing ad page

---
+++ src/components/PushAd.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const PushAd = () => {
+  return (
+    <div className='container'>
+        <div className='container--item'>
+            <div>
+                <h2>Lets help you reach more Customers/Visitors and sell faster with as low as N300</h2>
+                <h3>Pay Now and make sales::</h3>
+            </div>
+            <form action="" style={{width: "100%"}}>
+                <input type="text" name="" id="" placeholder='imamddahir@gmail.com'/>
+                <input type="text" name="" id="" placeholder='enter product url'/>
+                <i>you can get product url by clicking on the product and copying the url from the browser address bar tab </i>
+                <input type="submit" value="submit" name="" id="" placeholder='enter product url'/>
+            </form>
+        </div>
+    </div>
+  )
+}
