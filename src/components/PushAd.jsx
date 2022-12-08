a93747d65240c27c6267f9c8e4f3bf62eaae6c9e
eimaam <imamddahir@gmail.com>
2022-12-08 12:57:21 +0100

    fix: changed placeholder to display email instead of user obj

--- src/components/PushAd.jsx
+++ src/components/PushAd.jsx
@@ -11,7 +11,7 @@ export const PushAd = () => {
                 <h3>Pay Now and make sales::</h3>
             </div>
             <form action="" style={{width: "100%"}}>
-                <input type="text" name="" id="" placeholder={user}/>
+                <input type="text" name="" id="" placeholder={user.email}/>
                 <input type="text" name="" id="" placeholder='enter product url'/>
                 <i>you can get product url by clicking on the product and copying the url from the browser address bar tab </i>
                 <input type="submit" value="submit" name="" id="" placeholder='enter product url'/>
